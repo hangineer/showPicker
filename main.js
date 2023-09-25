c1061b8500ba90d6ae381c5cff7d638839276204
王涵 <hannahwang@locahost.localdomain>
2023-09-26 01:30:31 +0800

    feat: add other HTMLInputElement API

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-document.getElementById('pickBtn').addEventListener('click', (e) => {
-    e.preventDefault();
+document.getElementById('pickBtn').addEventListener('click', () => {
   //   Test
   if ("showPicker" in HTMLInputElement.prototype) {
     console.log("yes");
     try {
+      document.getElementById('inputDatalist').showPicker();
         // document.getElementById('inputColor').showPicker();
-        document.getElementById('inputDatalist').showPicker();
-        // document.getElementById('inDate').showPicker();
-        // document.getElementById('inTime').showPicker();
+        // document.getElementById('inputDate').showPicker();
+        // document.getElementById('inputTime').showPicker();
         // document.getElementById('inputLocal').showPicker();
-        // document.getElementById('inMonth').showPicker();
-        // document.getElementById('inWeek').showPicker();
+        // document.getElementById('inputMonth').showPicker();
+        // document.getElementById('inputWeek').showPicker();
         // document.getElementById('inputFile').showPicker();
       } catch (err) {
         switch (err.type) {
           case 'InvalidStateError':
-          //element is not mutable (disabled)
+          // element is not mutable (disabled)
           case 'NotAllowedError':
-          //didn't use transient activation
+          // didn't use transient activation
           case 'SecurityError':
-            //cross-origin issue involving iframes
+            // cross-origin issue involving iframe
             console.log({ err });
             break;
           default:
@@ -27,18 +26,34 @@ document.getElementById('pickBtn').addEventListener('click', (e) => {
         }
       }
   }
-
-  // input[type=color]
-  // const inputColor = document.querySelector('input[type=color]');
-  // inputColor.showPicker();
-
-  // // EyeDropper
-  // document.querySelector('.button--eyedropper')
-  //   .addEventListener('click',
-  //     async () => {
-  //       const eyeDropper = new EyeDropper();
-  //       const result = await eyeDropper.open();
-  //       const colorHexValue = result.sRGBHex;
-  //     }
-  //   )
 })
+
+
+// stepUp & stepDown
+const inputNumber = document.querySelector('input[type=number]');
+// Plus
+document.getElementById('btnPlus').addEventListener('click', () => {
+  inputNumber.stepUp();
+})
+
+// Minus
+document.getElementById('btnMinus').addEventListener('click', () => {
+  inputNumber.stepDown();
+});
+
+// select()
+const inputSelectContent = document.querySelector('.input-selectContent');
+document.getElementById('selectBtn').addEventListener('click', () => {
+  inputSelectContent.select();
+});
+
+
+// EyeDropper
+// document.querySelector('.button--eyedropper')
+//   .addEventListener('click',
+//     async () => {
+//       const eyeDropper = new EyeDropper();
+//       const result = await eyeDropper.open();
+//       const colorHexValue = result.sRGBHex;
+//     }
+//   )
