40f403cb9b7d68c6a91ee873149b988f76de18e4
王涵 <hannahwang@locahost.localdomain>
2023-10-05 09:46:44 +0800

    fix: code formatted

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 document.getElementById('pickBtn').addEventListener('click', () => {
   //   Test
-  if ("showPicker" in HTMLInputElement.prototype) {
-    console.log("yes");
+  if ('showPicker' in HTMLInputElement.prototype) {
+    console.log('yes');
     try {
-      document.getElementById('inputDatalist').showPicker();
+      // document.getElementById('inputDatalist').showPicker();
         // document.getElementById('inputColor').showPicker();
         // document.getElementById('inputDate').showPicker();
         // document.getElementById('inputTime').showPicker();
         // document.getElementById('inputLocal').showPicker();
         // document.getElementById('inputMonth').showPicker();
         // document.getElementById('inputWeek').showPicker();
-        // document.getElementById('inputFile').showPicker();
+        document.getElementById('inputFile').showPicker();
       } catch (err) {
         switch (err.type) {
           case 'InvalidStateError':
@@ -49,8 +49,8 @@ document.getElementById('selectBtn').addEventListener('click', () => {
 
 
 // EyeDropper
-const resultElement = document.getElementById("eyeDropperResult");
-const resultHexElement = document.getElementById("eyeDropperHex");
+const resultElement = document.getElementById('eyeDropperResult');
+const resultHexElement = document.getElementById('eyeDropperHex');
 document.getElementById('btnEyeDropper').addEventListener('click', async () => {
   const eyeDropper = new EyeDropper();
   const result = await eyeDropper.open();
